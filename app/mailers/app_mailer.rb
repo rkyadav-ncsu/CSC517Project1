72c74c1150f7866c866af251d9fdefd21e6b3247
William Galliher <wpgallih@ncsu.edu>
2015-02-13 18:50:44 -0500

    Typos in the gemfile

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -3,7 +3,7 @@ class AppMailer < ActionMailer::Base
   def new_user_email(user)
     @user = user
     @url  = 'http://localhost:3000/'
-    delivery_options = { user_name: 'info@sukuya.com',password: 'WhatDoIKnow5683$',domain:'sukuya.com',authentication:'plain',address: 'smtp.gmail.com', port:'587'}
+    delivery_options = { user_name: 'info@sukuya.com',password: 'PassProWhat1673$',domain:'sukuya.com',authentication:'plain',address: 'smtp.gmail.com', port:'587'}
     mail(to: @user.email,subject: "User account is successfully created",delivery_method_options: delivery_options, template_name:'new_user' )
   end
 end
